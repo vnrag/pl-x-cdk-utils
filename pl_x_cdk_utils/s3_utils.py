e94d6763ca2022cbad162679334706ce8b4a4323
ALo - Amira Imam <amiralotfy.4@gmail.com>
2022-03-18 12:44:35 +0100

    added retrieve s3 buckets by arn

--- pl_x_cdk_utils/s3_utils.py
+++ pl_x_cdk_utils/s3_utils.py
@@ -16,3 +16,18 @@ def get_bucket_object_from_name(construct, bucket_name):
     bucket_object = s3.Bucket.from_bucket_name(
         construct, f"profile-for-bucket-{bucket_name}", bucket_name)
     return bucket_object
+
+
+def get_bucket_object_from_arn(construct, bucket_arn):
+    """
+    Get bucket object with provided bucket arn for cross-account buckets
+    :param construct: object
+                      Stack Scope
+    :param bucket_arn: string
+                        ARN of the bucket
+    :return: object
+             Bucket object
+    """
+    bucket_object = s3.Bucket.from_bucket_arn(
+        construct, f"profile-for-bucket-{bucket_arn}", bucket_arn)
+    return bucket_object
