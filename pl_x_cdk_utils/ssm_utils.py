202d08e17e75dce81ba629251ab26af9832e7ddf
ALo - Amira Imam <amiralotfy.4@gmail.com>
2022-03-18 10:56:11 +0100

    adjusted ssm retrieve parameter

--- pl_x_cdk_utils/ssm_utils.py
+++ pl_x_cdk_utils/ssm_utils.py
@@ -34,7 +34,7 @@ def put_ssm_string_parameter(construct, parameter_name, string_value,
     return res
 
 
-def retrieve_ssm_string_parameter_value(construct, parameter_name):
+def retrieve_ssm_string_parameter_value(construct, parameter_name, parameter=False):
     """
 
     :param construct: object
@@ -45,6 +45,9 @@ def retrieve_ssm_string_parameter_value(construct, parameter_name):
              AWS SSM parameter token object used as string during synth
     """
     val = ssm.StringParameter.from_string_parameter_attributes(
+        construct, f"profile-for-ssm-retrieve-{parameter_name}",
+        parameter_name=parameter_name) if parameter else \
+        ssm.StringParameter.from_string_parameter_attributes(
         construct, f"profile-for-ssm-retrieve-{parameter_name}",
         parameter_name=parameter_name).string_value
 
