10da5b7354e2fc05241d8ce2e011a90bbb8e1be7
pcx <chettripratima@gmail.com>
2022-03-24 13:30:58 +0100

    sns utils added: sns topic

---
+++ pl_x_cdk_utils/sns_utils.py
@@ -0,0 +1,28 @@
+from aws_cdk import sns
+
+
+def get_sns_topic(construct, topic_name, display_name="Subscription Topic",
+                  fifo=False):
+    """
+    Create SNS topic
+    Parameters
+    ----------
+    construct : object
+                Stack Scope
+    topic_name : string
+                 Name for the topic
+    display_name : string
+                   Display name for the topic
+    fifo : bool
+           Boolean for the fifo
+
+    Returns
+    -------
+    SNS topic object
+    """
+    topic = sns.Topic(
+        construct, f"Topic{topic_name}",
+        display_name=display_name,
+        fifo=fifo,
+        topic_name=topic_name)
+    return topic
