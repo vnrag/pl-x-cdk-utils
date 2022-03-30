5be7d02219e885469232446acc0aa18dddcb61e7
pcx <chettripratima@gmail.com>
2022-03-30 20:33:46 +0200

    try except in change_s3_policy

--- pl_x_cdk_utils/boto3_utils.py
+++ pl_x_cdk_utils/boto3_utils.py
@@ -18,22 +18,26 @@ def get_ssm_value(ssm_param, region='eu-central-1', aws_credentials=None):
         access_key = aws_credentials['Credentials']['AccessKeyId']
         secret_key = aws_credentials['Credentials']['SecretAccessKey']
         session_token = aws_credentials['Credentials']['SessionToken']
-        ssm_client = boto3.client('ssm', aws_access_key_id=access_key,
-                                  aws_secret_access_key=secret_key,
-                                  aws_session_token=session_token,
-                                  region_name=region)
+        ssm_client = boto3.client(
+            'ssm', aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            aws_session_token=session_token,
+            region_name=region
+            )
     else:
         ssm_client = boto3.client('ssm', region_name=region)
     target_val = ssm_client.get_parameter(
-        Name=ssm_param,
-        WithDecryption=False
-    )
+            Name=ssm_param,
+            WithDecryption=False
+            )
 
     return target_val["Parameter"]["Value"]
 
 
-def trigger_glue_crawler(gc_name, aws_credentials=None,
-                         region_name='eu-central-1'):
+def trigger_glue_crawler(
+        gc_name, aws_credentials=None,
+        region_name='eu-central-1'
+        ):
     """
     :param gc_name: string
                     Glue-Crawler name
@@ -48,10 +52,12 @@ def trigger_glue_crawler(gc_name, aws_credentials=None,
         access_key = aws_credentials['Credentials']['AccessKeyId']
         secret_key = aws_credentials['Credentials']['SecretAccessKey']
         session_token = aws_credentials['Credentials']['SessionToken']
-        client = boto3.client('glue', aws_access_key_id=access_key,
-                              aws_secret_access_key=secret_key,
-                              aws_session_token=session_token,
-                              region_name=region_name)
+        client = boto3.client(
+            'glue', aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            aws_session_token=session_token,
+            region_name=region_name
+            )
     else:
         client = boto3.client('glue', region_name=region_name)
 
@@ -72,9 +78,9 @@ def invoke_lambda(lambda_func, payload={}):
     """
     boto_conn = boto3.client("lambda")
     resp = boto_conn.invoke(
-        FunctionName=lambda_func,
-        Payload=json.dumps(payload)
-    )
+            FunctionName=lambda_func,
+            Payload=json.dumps(payload)
+            )
     out = json.loads(resp["Payload"].read())
     return out
 
@@ -84,11 +90,15 @@ def upload_file_to_s3(file_path, bucket_name, prefix, region='eu-central-1'):
     s3_client = boto3.client("s3", region_name=region)
     try:
         response = s3_client.upload_file(file_path, bucket_name, prefix)
-        print(f"----- File Uploaded to Bucket: {bucket_name}, "
-              f"Path: {prefix} -----")
+        print(
+            f"----- File Uploaded to Bucket: {bucket_name}, "
+            f"Path: {prefix} -----"
+            )
     except Exception as e:
-        print(f"----- Error on Uploading File to Bucket: {bucket_name}, "
-              f"Error: {e} -----")
+        print(
+            f"----- Error on Uploading File to Bucket: {bucket_name}, "
+            f"Error: {e} -----"
+            )
 
 
 def get_files_under_given_bucket_prefix(bucket, prefix):
@@ -111,8 +121,10 @@ def get_files_under_given_bucket_prefix(bucket, prefix):
     return files_path_list
 
 
-def get_cross_account_credentials(account_id, role_name,
-                                  region_name='eu-central-1'):
+def get_cross_account_credentials(
+        account_id, role_name,
+        region_name='eu-central-1'
+        ):
     """
     :param account_id: string
                     AWS account Id
@@ -126,15 +138,17 @@ def get_cross_account_credentials(account_id, role_name,
     sts_connection = boto3.client('sts', region_name=region_name)
     role_arn = f"arn:aws:iam::{account_id}:role/{role_name}"
     aws_credentials = sts_connection.assume_role(
-        RoleArn=role_arn,
-        RoleSessionName=f"cross_acct_lambda_{role_name}"
-    )
+            RoleArn=role_arn,
+            RoleSessionName=f"cross_acct_lambda_{role_name}"
+            )
     return aws_credentials
 
 
-def initiate_quicksight_ingestion(dataset_id,quicksight_account_id,
-                                  aws_credentials=None,
-                                  region_name='eu-central-1'):
+def initiate_quicksight_ingestion(
+        dataset_id, quicksight_account_id,
+        aws_credentials=None,
+        region_name='eu-central-1'
+        ):
     """
     :param dataset_id: string
                        Dataset Id for Quicksight dataset
@@ -151,25 +165,28 @@ def initiate_quicksight_ingestion(dataset_id,quicksight_account_id,
         access_key = aws_credentials['Credentials']['AccessKeyId']
         secret_key = aws_credentials['Credentials']['SecretAccessKey']
         session_token = aws_credentials['Credentials']['SessionToken']
-        client = boto3.client('quicksight', aws_access_key_id=access_key,
-                              aws_secret_access_key=secret_key,
-                              aws_session_token=session_token,
-                              region_name=region_name)
+        client = boto3.client(
+            'quicksight', aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            aws_session_token=session_token,
+            region_name=region_name
+            )
     else:
         client = boto3.client('quicksight', region_name=region_name)
 
     ingestion_id = str(uuid.uuid4())
     response = client.create_ingestion(
-        DataSetId=dataset_id,
-        IngestionId=ingestion_id,
-        AwsAccountId=quicksight_account_id
-    )
+            DataSetId=dataset_id,
+            IngestionId=ingestion_id,
+            AwsAccountId=quicksight_account_id
+            )
     return response, ingestion_id
 
 
 def change_s3_policy(
         sid, bucket_name, principal_arn, actions,
-        resources=['*'], region='eu-central-1', aws_credentials=None):
+        resources=['*'], region='eu-central-1', aws_credentials=None
+        ):
     """
     Add new bucket policy to existing bucket
 
@@ -198,54 +215,65 @@ def change_s3_policy(
         access_key = aws_credentials['Credentials']['AccessKeyId']
         secret_key = aws_credentials['Credentials']['SecretAccessKey']
         session_token = aws_credentials['Credentials']['SessionToken']
-        s3 = boto3.client('s3', aws_access_key_id=access_key,
-                          aws_secret_access_key=secret_key,
-                          aws_session_token=session_token,
-                          region_name=region)
+        s3 = boto3.client(
+            's3', aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            aws_session_token=session_token,
+            region_name=region
+            )
     else:
         s3 = boto3.client('s3', region_name=region)
 
-    current_policy = json.loads(s3.get_bucket_policy(
-        Bucket=bucket_name)['Policy'])
+    current_sids = []
+    try:
+        current_policy = json.loads(
+                s3.get_bucket_policy(
+                        Bucket=bucket_name
+                        )['Policy']
+                )
+        # check duplicated sid
+        for statement in current_policy["Statement"]:
+            current_sids.append(statement["Sid"])
+    except Exception as e:
+        print(f"----- Error on loading current policies. Error: {e} -----")
+        current_policy = dict()
 
     new_policy_statement = {
-        "Sid": sid,
-        "Effect": "Allow",
-        "Principal": {
-            "AWS": principal_arn
-        },
-        "Action": actions,
-        "Resource": resources
-    }
+            "Sid": sid,
+            "Effect": "Allow",
+            "Principal": {
+                    "AWS": principal_arn
+                    },
+            "Action": actions,
+            "Resource": resources
+            }
 
-    # check duplicated sid
-    current_sids = []
-    for statement in current_policy["Statement"]:
-        current_sids.append(statement["Sid"])
-
-    working_statements = current_policy["Statement"]
-
+    working_statements = current_policy["Statement"] if \
+        'Statement' in current_policy else dict()
     # add only the new statement if it doesn't exist before
     if sid not in current_sids:
         working_statements.append(new_policy_statement)
     else:
         return {
-            "message": f"couldn't add the new statement. "
-                       f"sid: {sid} already exists in bucket policy, \
+                "message": f"couldn't add the new statement. "
+                           f"sid: {sid} already exists in bucket policy, \
                 please change it and try again",
-            "ResponseMetadata": {"HTTPStatusCode": 400}
-        }
-    bucket_policy = {'Version': '2012-10-17',
-                     "Statement": list(working_statements)}
+                "ResponseMetadata": {"HTTPStatusCode": 400}
+                }
+    bucket_policy = {
+            'Version': '2012-10-17',
+            "Statement": list(working_statements)
+            }
 
     response = s3.put_bucket_policy(
-        Bucket=bucket_name, Policy=json.dumps(bucket_policy))
+            Bucket=bucket_name, Policy=json.dumps(bucket_policy)
+            )
     # check on response code
     if response["ResponseMetadata"]["HTTPStatusCode"] == 204:
         return {
-            "message": (f"{bucket_name} bucket policy is "
-                        f"updated successfully"),
-            "ResponseMetadata": {"HTTPStatusCode": 204}
-        }
+                "message": (f"{bucket_name} bucket policy is "
+                            f"updated successfully"),
+                "ResponseMetadata": {"HTTPStatusCode": 204}
+                }
     else:
         return response
