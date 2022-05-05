b4ccbc31ee9f68da17decd3ea2fa2a555b4dd2dc
ALo - Amira Imam <amiralotfy.4@gmail.com>
2022-05-05 16:42:38 +0200

    added sns functionality and upgraded cdk version

--- pl_x_cdk_utils/sns_utils.py
+++ pl_x_cdk_utils/sns_utils.py
@@ -1,8 +1,9 @@
 from aws_cdk import aws_sns as sns
 
 
-def get_sns_topic(construct, topic_name, display_name="Subscription Topic",
-                  fifo=False, id=None):
+def get_sns_topic(
+    construct, topic_name, display_name="Subscription Topic", fifo=False, id=None
+):
     """
     Create SNS topic
     Parameters
@@ -23,9 +24,32 @@ def get_sns_topic(construct, topic_name, display_name="Subscription Topic",
     """
     param_id = id if id else f"Topic{topic_name}"
     topic = sns.Topic(
-        construct, param_id,
-        display_name=display_name,
-        fifo=fifo,
-        topic_name=topic_name)
+        construct, param_id, display_name=display_name, fifo=fifo, topic_name=topic_name
+    )
+
+    return topic
+
+
+def get_sns_topic_from_arn(construct, topic_name, id=None):
+    """
+    Create SNS topic
+    Parameters
+    ----------
+    construct : object
+                Stack Scope
+    topic_name : string
+                 Name for the topic
+    id: string
+        Id to reuse the existing resource if available
+    Returns
+    -------
+    SNS topic object
+    """
+    param_id = id if id else f"Topic{topic_name}"
+    topic = sns.Topic.from_topic_arn(
+        construct,
+        id=param_id,
+        topic_arn=f"arn:aws:sns:{construct.region}:{construct.account}:{topic_name}",
+    )
 
     return topic
