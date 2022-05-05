50e407dd087a252939e7b8ea22a7e5137724bd30
ALo - Amira Imam <amiralotfy.4@gmail.com>
2022-05-05 16:47:51 +0200

    upgraded cdk version

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
 # Package meta-data.
-NAME = 'pl_x_cdk_utils'
-DESCRIPTION = 'Public VNR package with AWS-CDK built in functions and ' \
-              'boto3 helpers'
-URL = 'https://github.com/vnrag/pl-x-cdk-utils'
-VERSION = '0.1.0'
-REQUIRES_PYTHON = '>=3.8.0'
+NAME = "pl_x_cdk_utils"
+DESCRIPTION = "Public VNR package with AWS-CDK built in functions and " "boto3 helpers"
+URL = "https://github.com/vnrag/pl-x-cdk-utils"
+VERSION = "0.1.0"
+REQUIRES_PYTHON = ">=3.8.0"
 
 # Packages required
 REQUIRED = [
-    'boto3',
-    'aws-cdk-lib==2.14.0',
-    'constructs>=10.0.0,<11.0.0',
-    'aws_cdk.aws_glue_alpha'
+    "boto3",
+    "aws-cdk-lib==2.21.1",
+    "constructs>=10.0.0,<11.0.0",
+    "aws_cdk.aws_glue_alpha",
 ]
 
 
@@ -22,16 +21,16 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=DESCRIPTION,
-    long_description_content_type='text/x-rst',
+    long_description_content_type="text/x-rst",
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    license='MIT',
+    license="MIT",
     packages=find_packages(exclude=("test",)),
     install_requires=REQUIRED,
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.9",
-        'License :: OSI Approved :: MIT License',
-        "Operating System :: OS Independent"
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
