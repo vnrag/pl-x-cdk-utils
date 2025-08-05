e7c8c8ad3d1820dd2843bcf091d0a967d8cf8fca
DA - David Amornvuttkul <david.amornvuttkul@gmx.de>
2025-08-05 11:47:15 +0200

    DATAPM-1208 - Added VPC- and Subnet lookup also adjusted StepFunction EMR Cluster creation Added functions to retrieve default VPC and subnets in ec2_utils.py; updated get_cluster_start_json to handle optional ec2_subnet and ec2_subnets in step_function_json_utils.py.

--- pl_x_cdk_utils/ec2_utils.py
+++ pl_x_cdk_utils/ec2_utils.py
@@ -23,6 +23,38 @@ def retrieve_vpc(construct, vpc_id, id=None):
     return vpc
 
 
+def get_default_vpc(construct):
+    """
+    Get the default VPC for the account
+
+    Parameters
+    ----------
+    construct : object
+                Stack Scope
+
+    """
+    vpc = ec2.Vpc.from_lookup(
+        construct,
+        "default-vpc",
+        is_default=True,
+        vpc_name="default"
+    )
+    return vpc
+
+
+def get_subnets(construct, vpc_id, subnet_type="public"):
+    """
+    Get the subnets for the VPC
+    """
+    vpc = retrieve_vpc(construct, vpc_id)
+    if subnet_type == "public":
+        return vpc.public_subnets
+    elif subnet_type == "private":
+        return vpc.private_subnets
+    else:
+        raise ValueError(f"Invalid subnet type: {subnet_type}")
+
+
 def get_ecs_instance(instance_type):
     """
 
